635a425ede7649f034310784aa4291bd202b3142
abdullah <binmazi22@gmail.com>
2021-07-10 09:53:12 +0530

    Initial commit

---
+++ src/main/kotlin/MainFunction.kt
@@ -0,0 +1,13 @@
+import FunctionVariablesStringInterpolation.FuncVariablesStringinterpolation
+
+fun main() {
+
+    val obj = FuncVariablesStringinterpolation(5, 2)
+    var l = obj.l
+    var b = obj.b
+
+    obj.areaOfRectangle(l,b)
+    obj.sumOfIntegers(5,10)
+
+
+}
---
+++ src/main/kotlin/functionVariablesStringInterpolation/FuncVariablesStringnterpolation.kt
@@ -0,0 +1,20 @@
+package FunctionVariablesStringInterpolation
+
+class FuncVariablesStringinterpolation(var length: Int, var breadth: Int) {
+
+    var l: Int = length
+    var b: Int = breadth
+
+
+    fun sumOfIntegers(num1 :Int, num2: Int){
+        /*String Templates or String Interpolation to extract or evaluate the statement and concatenate with String*/
+     print("the sum of $num1 and $num2 is : ${num1 + num2}")
+    }
+
+    fun areaOfRectangle(len :Int, bre: Int){
+        /*String Templates or String Interpolation to extract or evaluate the statement and concatenate with String
+        * we can avoid using + operator*/
+        println("The area of rectangle $len and $bre is : ${len * bre}")
+    }
+
+}
