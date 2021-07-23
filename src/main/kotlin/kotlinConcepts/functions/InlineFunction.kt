3831b726275f2fcae56f879149228b6854dc7ce1
abdullah <binmazi22@gmail.com>
2021-07-23 18:41:47 +0530

    minor improvements in in inline function with noinline modifier

--- src/main/kotlin/kotlinConcepts/functions/InlineFunction.kt
+++ src/main/kotlin/kotlinConcepts/functions/InlineFunction.kt
@@ -11,13 +11,19 @@ class InlineFunction {
         println(sum)
     }
 
-    inline fun getAreaOfTriangle(l: Float, b: Float, noinline getTriangle: (Float,Float) ->Float){
+    inline fun getAreaOfTriangle(l: Float,
+                                 b: Float,
+                                 squareRootOfNumber: (Int) -> Int,
+                                 noinline getTriangle: (Float,Float) ->Float,){
 
         //inline function with noinline modifier
         //only applicable to inline function otherwise ,will get compile time error
+        //if you have single functional parameter do use noinline for single function parameter
+        //use it when u have multiple functional parameter and want specific one to be not inlined
 
         println("inline function with specific lambda parameter as noinline")
         val triangle = getTriangle(l,b) //
+        val square = squareRootOfNumber(5)
         println(triangle)
     }
 }
@@ -30,5 +36,7 @@ fun main(){
 
     val triangleLambda : (Float,Float) -> Float = { l: Float, b:Float -> l*b }
 
-    inlineFunction.getAreaOfTriangle(5.0f,1.0f, triangleLambda)
+    val square: (Int) -> Int = {p ->  p*p}
+
+    inlineFunction.getAreaOfTriangle(5.0f,1.0f, square,triangleLambda)
 }
